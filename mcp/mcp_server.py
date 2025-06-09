e9705eb1984c39fdfca005ff93464f4be2124054
shinyEazy <tanhngusi39@gmail.com>
2025-06-09 17:35:21 +0700

    feat + refactor: add mcp server for agent

---
+++ mcp/mcp_server.py
@@ -0,0 +1,18 @@
+from mcp.server.fastmcp import FastMCP
+from tools.image_to_base64 import ImageToBase64
+from tools.get_data_in_csv import GetDataInCsv
+
+mcp = FastMCP("MCP Server")
+
+@mcp.tool()
+def image_to_base64(image_path: str) -> str:
+    """Convert image to base64"""
+    return ImageToBase64().image_to_base64(image_path)
+
+@mcp.tool()
+def get_data_in_csv(csv_path: str) -> str:
+    """Get the first row of data from the CSV file (excluding header)"""
+    return GetDataInCsv().get_data_in_csv(csv_path)
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
